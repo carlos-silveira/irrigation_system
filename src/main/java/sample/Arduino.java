a74107d2463c052016a3c203cdf487a7b1a794c7
Cosme Escobedo <comeoes@gmail.com>
2019-05-28 19:08:11 -0600

    hice lo de arduino

--- src/main/java/sample/Arduino.java
+++ src/main/java/sample/Arduino.java
@@ -6,13 +6,19 @@ import com.fazecast.jSerialComm.SerialPortEvent;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.ResultSet;
-import java.time.LocalDate;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 public class Arduino {
     private SerialPort comPort;
     private ConexionMySQL conexion;
+
+    private  int ultimoVolumen = 0;
+    private String ultimaFecha = "";
+
+    private Boolean encender = false;
 
     public Arduino(ConexionMySQL conexionMySQL){
         conexion = conexionMySQL;
@@ -22,6 +28,38 @@ public class Arduino {
             e.printStackTrace();
         }
         getValueSerial();
+        Thread t = new Thread(() -> {
+            while (true) {
+                revisarComunicacion();
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+        t.start();
+    }
+
+    private void revisarComunicacion() {
+        ResultSet r  =conexion.consultar("Select * from comunicacion where id = 1");
+        try {
+            if(r.next()){
+                boolean encenderDB = r.getBoolean(2);
+                if(encenderDB != encender) {
+                    OutputStream out = comPort.getOutputStream();
+                    out.write("a".getBytes());
+                    out.close();
+                    encender = encenderDB;
+                }
+                System.out.println("Encender: "+encender);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void getValueSerial(){
@@ -39,7 +77,7 @@ public class Arduino {
                 if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
                     return;
                 InputStream in = comPort.getInputStream();
-                byte[] buffer = new byte[4];
+                byte[] buffer = new byte[8];
                 String message = "";
                 int len = 0;
                 try {
@@ -52,8 +90,30 @@ public class Arduino {
                 }
                 try {
 //                    voltage = Integer.parseInt(message.trim());
-                    if(!message.isEmpty()) {
-                        ResultSet resultSet = conexion.consultar("INSERT INTO estado VALUES(1, 80.00, 0, 1) ON DUPLICATE KEY UPDATE humedad=80.00, lluvia=0, encendido=1");
+                    String[] datos = message.split("\\|");
+
+                    if(datos.length == 4) {
+                        ResultSet resultSet = conexion.consultar("INSERT INTO estado VALUES(1," +datos[1]+ "," +datos[2]+ ","+datos[0]+") ON DUPLICATE KEY UPDATE humedad=" +datos[1]+ ", lluvia=" +datos[2]+ ", encendido="+datos[0]);
+                        int volumen = Integer.parseInt(datos[3]) - ultimoVolumen;
+                        LocalDateTime fecha = LocalDateTime.now();
+                        String fechaStr = fecha.toString().split(":")[0];
+                        System.out.println(fecha.toString().split(":")[0]);
+                        if(ultimaFecha.equals(fechaStr)) {
+                            conexion.consultar("Update consumo set litros = litros + "+volumen+" where fecha = '"+fechaStr+":00'");
+                        } else {
+                            ResultSet r = conexion.consultar("select * from consumo where fecha = '"+fechaStr+":00'");
+                            r.last();
+                            if(r.getRow() > 0) {
+                                System.out.println("Actualizando");
+                                conexion.consultar("Update consumo set litros = litros + "+volumen+" where fecha = '"+fechaStr+":00'");
+                            } else {
+                                System.out.println("Insertando");
+                                conexion.consultar("insert into consumo values (0 , '"+fechaStr+":00', " +volumen+")");
+
+                            }
+                        }
+                        ultimoVolumen += volumen;
+                        ultimaFecha = fechaStr;
                         System.out.println("Mensaje: " +message.trim());
                     }
 
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        new Arduino(new ConexionMySQL("root", "", "sistemaRiego", "localhost", 3306));
+//        new Arduino(new ConexionMySQL("root", "", "sistemaRiego", "localhost", 3306));
         launch(args);
     }
 }
